1b6e1033515cfbc5b58e4f3ede55ef7c1ac08649
Ary Mesquita <traderary@gmail.com>
2021-09-06 19:03:28 +0100

    Setup Heroku, composer, npm, key generate

---
+++ app/Models/Event.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title','city','private','description','image'];
+    protected $casts = ['items' => 'array'];
+    protected $dates = ['date'];
+
+    //Relative function update
+    protected $guarded = [];
+
+    public function user() {
+		return $this->belongsTo('App\Models\User');
+}
+	
+	public function users() {
+        return $this->belongsToMany('App\Models\User');
+    }
+
+}
+
+	
