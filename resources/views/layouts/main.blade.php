046e4f4a8678d6fd239b8e56b0bb0375c057032b
Ary Mesquita <traderary@gmail.com>
2021-09-17 18:26:33 +0100

    Mobile

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -19,10 +19,13 @@
     </head>
     <body>
         <header>
-        <nav class="navbar navbar-expand-lg navbar-light">
+        <nav class="navbar navbar-expand-sm navbar-light">
+          <button class="navbar-toggler" data-toggle="collapse" data-target="#navbar">
+              <span class="navbar-toggler-icon"></span>
+          </button>
           <div class="collapse navbar-collapse" id="navbar">
             <a href="/" class="navbar-brand">
-              <img src="/img/amweb_logo.png" alt="HDC Events">
+              <img src="/img/amweb_logo.png" alt="amweb">
             </a>
             <ul class="navbar-nav">
               <li class="nav-item">
@@ -69,7 +72,9 @@
     <footer>
         <p>Laravel Events &copy; 2021</p>
     </footer>
-    <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
     </body>
 
 </html>
